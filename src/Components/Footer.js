c35dee00fd60c1efc81bcf99a8efcf61d18218f7
DChirinos <dlchirinos87@gmail.com>
2023-07-31 16:06:13 -0400

    added main components to App.js and meta tags to index.html

---
+++ src/Components/Footer.js
@@ -0,0 +1,38 @@
+
+
+function Footer () {
+    return (
+        <>
+            <footer>
+                <section>
+                    <img src=""/>
+                </section>
+                <section>
+                    <h5>Doormat Nav</h5>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Menu</a></li>
+                        <li><a href="">Reservations</a></li>
+                        <li><a href="">Order Online</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h5>Contact Us</h5>
+                    <p>
+                        Address: 
+                        Phone:
+                        Email: <a></a>
+                    </p>
+                </section>
+                <section>
+                    <h5>Social Media Links</h5>
+                </section>
+
+            </footer>
+        </>
+    )
+}
+
+export default Footer; 
+
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+
+
+function Header () {
+    return (
+        <>
+            <header>
+                <img src="Logo.svg" alt="logo" />
+            </header>
+        </>
+    )
+}
+
+export default Header;
